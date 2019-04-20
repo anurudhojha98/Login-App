be384567db007bcaa82ebf89944e041109b7a7f7
anurudhojha98 <anurudhojha98@gmail.com>
2019-04-21 00:04:38 +0530

    first commit

---
+++ src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'kcTPdNaJtYu62s9FDto4ua0oJKm2qes3',
+  domain: 'anurudhojha.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+};
